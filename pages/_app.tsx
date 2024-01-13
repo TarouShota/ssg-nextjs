27264d2bf15dd94bbd05fa62af346e8f14662449
TarouShota <adilov_02@mail.ru>
2024-01-13 21:40:10 +0100

    remove unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,20 @@
 import type { AppProps } from 'next/app'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
 import Head from 'next/head'
-import { getLayout } from '@vercel/examples-ui'
+
 
 import '@vercel/examples-ui/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
 
   return (
     <>
       <Head>
         <title>
-          997 wacuum flask
+          705 wacuum flask
         </title>
 
-        <meta name="description" content="997 wacuum flask buy" />
-        <meta name="keywords" content="wacuum, wacuum flask, 997, czu, water, water bottle" />
+        <meta name="description" content="705 wacuum flask buy" />
+        <meta name="keywords" content="wacuum, wacuum flask, 705, czu, water, water bottle" />
         <meta name="author" content="Adylov Bekzot" />
       </Head>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
-import { Layout, Text, Page, Code, Link, List } from '@vercel/examples-ui'
+import { Layout, Link } from '@vercel/examples-ui'
 import { GetStaticProps } from 'next'
-import useSWR from 'swr'
-import { useEffect, useState } from 'react'
+
 
 interface Product {
   id: string
@@ -21,130 +20,25 @@ interface Props {
   product: Product
 }
 
-const fetcher = (input: RequestInfo, init?: RequestInit) =>
-  fetch(input, init).then((res) => res.json())
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       product: {
-        id: 'flask-997',
-        title: '997 Flask',
+        id: 'flask-705',
+        title: '705 Flask',
         image: '/bottle.jpg',
         price: 150,
         stock: 5,
-        name: "997 Vacuum Flask",
+        name: "Vacuum Flask",
 
-        description: "Experience perfection with the 997 Vacuum Flask. A sleek black design and cutting-edge insulation redefine on-the-go luxury.",
-        features: "Superior insulation, leak-proof, and effortlessly chic. The 997 Vacuum Flask is more than a beverage companion; it's an expression of your refined taste.",
+        description: "Experience perfection with the 705 Vacuum Flask. A sleek black design and cutting-edge insulation redefine on-the-go luxury.",
+        features: "Superior insulation, leak-proof, and effortlessly chic. The 705 Vacuum Flask is more than a beverage companion; it's an expression of your refined taste.",
         action: "Order Now"
       },
     },
   }
 }
-
-function ProductCard({ product }: Props) {
-  const [isAdded, toggleAdded] = useState<boolean>(false)
-  const { data: stock } = useSWR(`/api/product/${product.id}/stock`, fetcher, {
-    refreshInterval: 5000,
-  })
-  const isLoading = stock === undefined
-
-  useEffect(() => {
-    let timeout: NodeJS.Timeout
-
-    if (isAdded) {
-      timeout = setTimeout(() => {
-        toggleAdded(false)
-      }, 1000)
-    }
-
-    return () => clearTimeout(timeout)
-  }, [isAdded])
-
-  return (
-    <div className="w-full max-w-xl mx-auto">
-      <div className="ml-14 lg:ml-24 -mb-40 lg:-mb-56">
-        <Image
-          className="pointer-events-none"
-          alt={product.title}
-          src={product.image}
-          width="440"
-          height="440"
-          layout="responsive"
-        />
-      </div>
-      <section className="border border-gray-300 bg-white rounded-lg shadow-lg mt-16 w-full hover:shadow-2xl transition pt-16 lg:pt-24">
-        <div className="p-4 flex flex-col justify-center items-center border-b">
-          <div className="flex justify-between w-full items-baseline">
-            <div className="ml-4 mr-auto text-left flex flex-col">
-              <h4 className="font-semibold text-xl">{product.title}</h4>
-              <h5 className="text-gray-700">
-                {product.description} {isLoading ? `` : `(${stock} left)`}
-              </h5>
-            </div>
-            <h4 className="font-bold text-lg">USD {product.price}</h4>
-          </div>
-        </div>
-        <div className="p-4 gap-4 flex flex-col justify-center items-center border-b">
-          {isLoading ? (
-            <a
-              role="button"
-              className="py-4 px-6 text-lg w-full bg-gray-500 cursor-not-allowed disabled text-center text-white rounded-md"
-            >
-              Loading...
-            </a>
-          ) : (
-            <>
-              {isAdded ? (
-                <a
-                  role="button"
-                  className="py-4 px-6 text-lg w-full bg-green-500 text-center text-white rounded-md"
-                >
-                  Added!
-                </a>
-              ) : (
-                <>
-                  {stock > 0 ? (
-                    <a
-                      role="button"
-                      onClick={() => toggleAdded(true)}
-                      className="py-4 px-6 text-lg w-full bg-black text-center text-white hover:text-white rounded-md hover:bg-gray-900"
-                    >
-                      Add to cart
-                    </a>
-                  ) : (
-                    <a
-                      role="button"
-                      className="py-4 px-6 text-lg w-full bg-gray-500 cursor-not-allowed disabled text-center text-white rounded-md"
-                    >
-                      No stock available
-                    </a>
-                  )}
-                </>
-              )}
-            </>
-          )}
-        </div>
-      </section>
-    </div>
-  )
-}
-
-
-
-
-
-// function Home2() {
-//   return (
-
-//   )
-// }
-
-
-
-
-
 
 
 function Home({ product }: Props) {
@@ -152,7 +46,6 @@ function Home({ product }: Props) {
     <div className="min-h-screen h-full bg-black text-white  flex items-center justify-center">
       <div className="max-w-2xl text-center">
         <div className="mb-4">
-          {/* Adjust the width and height values as needed */}
           <Image
             src={product.image}
             alt={product.name}
